1f116ce8064031e0bebe15e7c1c405c8eb2e9b34
Muskaan-10 <muskaanhira2007@gmail.com>
2021-06-12 10:59:16 +0530

    synchronised ball

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
 var ball;
+var database;
 
 function setup(){
     createCanvas(500,500);
+    database=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var locInDb=database.ref("Ball/Position");
+    locInDb.on("value",readPosition);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+ database.ref("Ball/Position").set({
+     x:ball.x+x, y:ball.y+y
+ })
+
 }
+
+function readPosition(value){
+    var posInDb=value.val();
+    ball.x=posInDb.x;
+    ball.y=posInDb.y;
+}
+
+
